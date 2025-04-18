aa70cf009886a71a2745d70ce09c2fbdb5291470
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-18 19:12:06 +0300

    Added a checkout page

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -5,56 +5,81 @@ export default function CheckOut() {
 
     const context = useCartContext()
     console.log(context)
-    {Array.from(context.state).map(([key, value]) => (
-      console.log(key, value, "iterator")
-    ))}
+    let sub_total = 0
+    {Array.from(context.state).map(([key, value]) => {
+        sub_total += value[1]
+        console.log(key, value, "iterator")
+    })
 
-    const items = [
-        {
-            product_name: "something",
-            freqency: 10,
-            price: 200,
-        },
-        {
-            product_name: "something",
-            freqency: 10,
-            price: 200,
-        },
-    ];
+    }
+
+
+
+    const handleDelete = (product_name)=>{
+        context.dispatch({
+            type:"REMOVE_PRODUCT",
+            payload:{
+                product_name,
+            }
+        })
+    }
+
+    const handleSubmit = async ()=>{
+
+        const products = {};
+        for (const [key, value] of context.state) {
+            products[key] = value;
+        }
+
+        const post_options = {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(products)
+        }
+        const response = await fetch("http://localhost:8080/api/orders/create-checkout-session", post_options)
+        const json_response = await response.json()
+        window.location.href = json_response.url
+
+    }
+
     return (
         <div className="flex flex-col h-screen items-center gap-8 py-8 ">
             <div className="flex flex-col w-full items-center gap-3">
                 <h1 className="text-3xl font-semibold">Shopping Cart</h1>
-                {items.map((item, index) => (
-                    <div
-                        key={index}
-                        className="flex flex-col py-2 items-center justify-between rounded-md bg-white  border border-gray-300 w-1/3 "
-                    >
+
+                {Array.from(context.state).map(([key, value]) => (
+                    <div key={key}
+                         className="flex flex-col py-2 items-center justify-between rounded-md bg-white  border border-gray-300 w-1/3 ">
                         <div className="w-full px-5">
                             <div className="flex flex-row justify-between">
-                                <p className="font-semibold">{item.product_name} </p>
-                                <button className="cursor-pointer py-1 px-2 text-red-600">x</button>
+                                <p className="font-semibold">{key} </p>
+                                <button className="cursor-pointer py-1 px-2 text-red-600"
+                                        onClick={() => handleDelete(key)}>x
+                                </button>
                             </div>
                             <p className="flex flex-col justify-center">
-                                {item.freqency} * {item.price}   = ${item.price * item.freqency}{" "}
-                                <br />
+                                {value[0]} * {value[1]} = ${value[0] * value[1]}{" "}
+                                <br/>
                             </p>
                         </div>
                     </div>
+
                 ))}
-                {Array.from(context.state).map(([key, value]) => (
-                    <div key={key}>
-                        <p>{key}</p>
-                        <p>Hi There</p>
-                    </div>
-                ))}
-            </div>
-            <button
-                className="bg-yellow-400 w-1/3 font-semibold py-2 px-3
+                <div className="flex justify-between font-semibold w-1/3">
+                    <span>Subtotal:</span>
+                    <span>${sub_total}</span>
+                </div>
+                <button
+                    className="bg-yellow-400 w-1/3 font-semibold py-2 px-3
          transition delay-150 duration-300 ease-in-out hover:-translate-y-1 rounded-md"
-            >
-                CheckOut
-            </button>
+                    onClick={handleSubmit}
+                >
+                    CheckOut
+                </button>
+            </div>
+
         </div>
     );
 }
