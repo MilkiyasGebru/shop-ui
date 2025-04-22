5ce5b68ff03d8965ed49e218aedffa2c96b62ba7
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-22 21:25:47 +0300

    added a loading screen

---
+++ components/LoadingScreen.js
@@ -0,0 +1,7 @@
+export default function LoadingScreen() {
+    return (
+        <div className="h-screen w-full flex flex-col items-center justify-center bg-white">
+            <span className="loading loading-bars loading-xl"></span>
+        </div>
+    )
+}
