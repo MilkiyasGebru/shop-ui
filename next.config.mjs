0255e0d7786f9b0870a5cdea6e832835165a25e1
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-22 21:34:14 +0300

    Added res.cloudinary.com to the image domains in the next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['demo2.wpopal.com'],
-    }
+        domains: ['demo2.wpopal.com','res.cloudinary.com'],
+    },
 };
 
 export default nextConfig;
